9e406f8c6f78db6701d5fe9e1e1277e377f92c11
SBorisoglebsky <SBorisoglebsky@gmail.com>
2021-09-04 20:19:56 +0300

    fix according recommendations

--- src/test/java/cloud/autotests/pages/TestCasesTable.java
+++ src/test/java/cloud/autotests/pages/TestCasesTable.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.ElementsCollection;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class TestCasesTable {
@@ -13,5 +14,9 @@ public class TestCasesTable {
     @Step("Check test cases table size")
     public void shouldHaveSize(int expectedSize) {
         rows.shouldHave(size(expectedSize));
+    }
+
+    public void navigateToTestByStatus(String statusName) {
+        $$(".LoadableTree__view > li").findBy(text(statusName)).click();
     }
 }
--- src/test/java/cloud/autotests/tests/TestPropertiesTest.java
+++ src/test/java/cloud/autotests/tests/TestPropertiesTest.java
@@ -2,9 +2,11 @@ package cloud.autotests.tests;
 
 import cloud.autotests.config.App;
 import cloud.autotests.data.MenuItem;
+import cloud.autotests.data.PropertyName;
 import cloud.autotests.helpers.WithLogin;
 import cloud.autotests.pages.ProjectPage;
 import cloud.autotests.pages.ProjectsTable;
+import cloud.autotests.pages.TestCasesTable;
 import cloud.autotests.pages.TestProperties;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.Test;
@@ -16,6 +18,7 @@ import static com.codeborne.selenide.Selenide.open;
 @Story("Properties of Tests")
 public class TestPropertiesTest extends TestBase{
     private static final String PROJECT_NAME = "teacher qa_guru_diplom_project";
+    private static final String STATUS_NAME = "New";
 
     @WithLogin
     @Test
@@ -24,22 +27,20 @@ public class TestPropertiesTest extends TestBase{
         ProjectPage projectPage = projectsTable.navigateTo(PROJECT_NAME);
         projectPage.getSidebar().navigateTo(MenuItem.TEST_CASES);
 
-        $$(".LoadableTree__view > li").findBy(text("New")).click();
+        TestCasesTable testCasesTable = new TestCasesTable();
+        testCasesTable.navigateToTestByStatus(STATUS_NAME);
 
         TestProperties testProperties = new TestProperties();
         testProperties.shouldHaveSize(8);
 
-        testProperties.shouldHaveElement("Tags");
-        testProperties.shouldHaveElement("History");
-        testProperties.shouldHaveElement("Issues links");
-        testProperties.shouldHaveElement("Members");
-        testProperties.shouldHaveElement("Fields");
-        testProperties.shouldHaveElement("Mutes");
-        testProperties.shouldHaveElement("Relations");
-        testProperties.shouldHaveElement("Description");
-        testProperties.shouldHaveElement("Scenario");
-        testProperties.shouldHaveElement("Comments");
-
+        testProperties.shouldHaveElement(PropertyName.TAGS.getDisplayedName());
+        testProperties.shouldHaveElement(PropertyName.HISTORY.getDisplayedName());
+        testProperties.shouldHaveElement(PropertyName.ISSUE_HISTORY.getDisplayedName());
+        testProperties.shouldHaveElement(PropertyName.MEMBERS.getDisplayedName());
+        testProperties.shouldHaveElement(PropertyName.FIELDS.getDisplayedName());
+        testProperties.shouldHaveElement(PropertyName.MUTES.getDisplayedName());
+        testProperties.shouldHaveElement(PropertyName.RELATIONS.getDisplayedName());
+        testProperties.shouldHaveElement(PropertyName.DESCRIPTIONS.getDisplayedName());
 
     }
 }
