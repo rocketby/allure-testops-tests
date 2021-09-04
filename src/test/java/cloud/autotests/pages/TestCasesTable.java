2877f6352866b5cb7de739c8351750dcb6bc535c
Rocket By <plplpl231212@gmail.com>
2021-09-04 12:17:30 +0300

    added changes according to import

--- src/test/java/cloud/autotests/pages/TestCasesTable.java
+++ src/test/java/cloud/autotests/pages/TestCasesTable.java
@@ -3,11 +3,12 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.$;
 
 public class TestCasesTable {
 
