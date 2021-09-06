a53a5913f4dbf02f42b1fead8a0314cf32dd31f4
vladimir klonin <vklonin@gmail.com>
2021-09-06 17:19:49 +0400

    cleared code

--- src/test/java/cloud/autotests/pages/MainPage.java
+++ src/test/java/cloud/autotests/pages/MainPage.java
@@ -2,15 +2,16 @@ package cloud.autotests.pages;
 
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
 import static io.qameta.allure.Allure.step;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class MainPage {
 
     @Step("Open main page")
     public void openPage() {
-                open("");
+        open("");
     }
 
     public ProjectsTable getProjectsTable() {
@@ -20,19 +21,18 @@ public class MainPage {
     @Step("Creating a new project from a main page")
     public ProjectPage createNewProject(String projectName) {
         step("Click on a button 'New project'", () ->
-            $("button.Button_style_success").click()
+                $("button.Button_style_success").click()
         );
-        step("Fill obligatory fields name with {projectName} and abbreviation with {projectAbbr}", () -> {
-            $("input[name=name]").setValue(projectName);
-        });
+        step("Fill obligatory fields name with {projectName} and abbreviation with {projectAbbr}", () ->
+                $("input[name=name]").setValue(projectName)
+        );
         step("Click submit, creating {projectName} project", () ->
-            $("button.Button_style_success[type=submit]").click()
+                $("button.Button_style_success[type=submit]").click()
         );
         return new ProjectPage();
     }
 
     public void filterProject(String projectName) {
-
         step("confirm the project {projectName} deletion", () -> {
             $("input.HomeLayout__search").setValue(projectName);
             sleep(500); // иначе через раз падает
